b844e4e20b3ab60f0509ded053bdb83192c17067
jedidiah <jedy_z_31@hotmail.com>
2021-03-31 17:48:30 -0500

    carpetas creadas

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+
+export const App = () => {
+    return <div> 
+
+
+
+
+    </div>
+}
+export default App
